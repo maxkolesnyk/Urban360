a3407085202b9b7564a245f0b75c3f6d3afa0bd7
Max Kolesnyk <max.kolesnyk1@gmail.com>
2026-04-26 17:23:13 +1000

    Logo: cache-bust to v2 filename (immutable CDN cache was serving stale white-bg version)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,7 +6,7 @@ export default function Logo({ className = "" }: LogoProps) {
   return (
     // eslint-disable-next-line @next/next/no-img-element
     <img
-      src="/images/urban360-logo.webp"
+      src="/images/urban360-logo-v2.webp"
       alt="Urban 360 Building Inspections"
       className={`block ${className}`}
       width={1200}
